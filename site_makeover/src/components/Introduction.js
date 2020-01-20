32fd18c764b4d26dd6e9129553b7aa32f9fef24e
Danielo814 <ojedadaniel92@gmail.com>
2020-01-19 18:45:24 -0800

    add all files

---
+++ site_makeover/src/components/Introduction.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="intro-container">
+      <div className="intro-text">
+        <p className="intro-line-spacing">Hello, i'm Daniel Ojeda.</p>
+        <p className="intro-line-spacing">I'm a web developer</p>
+        <p>living in San francisco.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
