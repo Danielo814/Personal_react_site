61fc3e05c0e5a672f0953b15d2c821ffd644642b
Danielo814 <ojedadaniel92@gmail.com>
2020-01-21 10:02:16 -0800

    add smooth scroll behavior on down arrow

--- site_makeover/src/components/Introduction.js
+++ site_makeover/src/components/Introduction.js
@@ -7,7 +7,13 @@ const Introduction = () => {
         <p className="intro-line-spacing">Hello, i'm Daniel Ojeda.</p>
         <p className="intro-line-spacing">I'm a web developer</p>
         <p>living in San francisco.</p>
-        <i class="angle down icon arrow-down"></i>
+        {/* <i class="angle down icon arrow-down"></i> */}
+        <div class="arrow-down">
+          <a className="arrow-transition" href="#project-container">
+            {" "}
+            &#8964;
+          </a>
+        </div>
       </div>
     </div>
   );
